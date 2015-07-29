399b05792204eba5a96600d404015fd2c74b567a
Jonathan Warner <jaxbot@gmail.com>
2015-07-28 23:48:23 -0700

    Flesh out nginx config

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 sudo apt-get update
-sudo apt-get install nginx
+sudo apt-get install nginx php5-fpm
 
 cp -R nginx/ /etc/nginx/
+ln -s /etc/nginx/sites-available/catch /etc/nginx/sites-enabled/catch
 ln -s /etc/nginx/sites-available/jme /etc/nginx/sites-enabled/jme
 ln -s /etc/nginx/sites-available/iv /etc/nginx/sites-enabled/iv
 ln -s /etc/nginx/sites-available/wph /etc/nginx/sites-enabled/wph
+ln -s /etc/nginx/sites-available/p18 /etc/nginx/sites-enabled/p18
+ln -s /etc/nginx/sites-available/knighthacks /etc/nginx/sites-enabled/knighthacks
+ln -s /etc/nginx/sites-available/nlqr /etc/nginx/sites-enabled/nlqr
+
