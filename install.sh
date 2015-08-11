73f1a8236e5c3e11b0ef043b6dc6e85c0430facf
Jonathan Warner <jaxbot@gmail.com>
2015-08-11 11:24:33 -0700

    Add more PHP packages

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install nginx php5-fpm
+sudo apt-get install nginx php5-fpm php5-cli php5-mysql
 
 cp -R nginx/ /etc/nginx/
 ln -s /etc/nginx/sites-available/catch /etc/nginx/sites-enabled/catch
@@ -10,3 +10,8 @@ ln -s /etc/nginx/sites-available/p18 /etc/nginx/sites-enabled/p18
 ln -s /etc/nginx/sites-available/knighthacks /etc/nginx/sites-enabled/knighthacks
 ln -s /etc/nginx/sites-available/nlqr /etc/nginx/sites-enabled/nlqr
 
+sudo /etc/init.d/nginx restart
+sudo /etc/init.d/php5-fpm start
+
+sudo mv init/* /etc/init/
+start p18
